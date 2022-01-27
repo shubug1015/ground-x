b52365b18267f86f7d6753b828f0699d8f504ab6
shubug1015 <shubug@naver.com>
2022-01-27 21:06:46 +0900

    header 삭제

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,14 +1,7 @@
-import Header from '@components/header';
-
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export default function Layout({ children }: LayoutProps) {
-  return (
-    <div>
-      <Header />
-      {children}
-    </div>
-  );
+  return <div>{children}</div>;
 }
