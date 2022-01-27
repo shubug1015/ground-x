4d78474bb6b4c20bededf88962f79df8f05d7fe7
shubug1015 <shubug@naver.com>
2022-01-28 08:51:02 +0900

    test

--- components/home/section1.tsx
+++ components/home/section1.tsx
@@ -1,4 +1,5 @@
 import { useScroll } from '@libs/useScroll';
+import { cls } from '@libs/utils';
 import type { NextPage } from 'next';
 import Image from 'next/image';
 // import { useEffect, useRef, useState } from 'react';
@@ -7,48 +8,49 @@ const Section1: NextPage = () => {
   const { y } = useScroll();
 
   return (
-    <div className='relatiave'>
-      <div
-        className='absolute left-0 flex justify-center items-center w-screen h-screen z-10'
-        style={{
-          top: -y / 2,
-          // opacity: y === 0 ? 1 : (460 - y) / 560,
-        }}
-        // className='relative flex justify-center items-center w-screen h-screen'
-      >
-        {/* Background Image */}
-        <div className='absolute top-0 left-0 w-full h-full'>
-          <div className='relative w-full h-full'>
-            <Image
-              src='/home/section1/background.png'
-              alt='Main Background Image'
-              layout='fill'
-              objectFit='cover'
-              priority
-            />
-          </div>
+    <div
+      className={cls(
+        y > 10 ? '-top-[500px] opacity-0' : '',
+        'absolute top-0 left-0 flex justify-center items-center w-screen h-screen z-10 transition-all duration-700'
+      )}
+      // style={{
+      //   top: y > 465 ? -465 : -y,
+      //   opacity: y > 465 ? 0 : y === 0 ? 1 : (460 - y) / 560,
+      // }}
+      // className='relative flex justify-center items-center w-screen h-screen'
+    >
+      {/* Background Image */}
+      <div className='absolute top-0 left-0 w-full h-full'>
+        <div className='relative w-full h-full'>
+          <Image
+            src='/home/section1/background.png'
+            alt='Main Background Image'
+            layout='fill'
+            objectFit='cover'
+            priority
+          />
         </div>
+      </div>
 
-        {/* Text */}
-        <div className='text-white z-[1] lg:w-[720px] md:w-[330px]'>
-          <h1 className='font-bold text-[68px] text-center lg:text-[48px] md:text-[36px] md:leading-[1.4]'>
-            누구나 크리에이터가 되는 세상
-          </h1>
-          <h2 className='font-medium text-[38px] text-center mt-[40px] lg:text-[28px] md:text-[18px] md:mt-[28px]'>
-            그라운드엑스가 NFT로 새로운 <br className='hidden md:block' />
-            디지털 세상을 만들어갑니다.
-          </h2>
-        </div>
+      {/* Text */}
+      <div className='text-white z-[1] lg:w-[720px] md:w-[330px]'>
+        <h1 className='font-bold text-[68px] text-center lg:text-[48px] md:text-[36px] md:leading-[1.4]'>
+          누구나 크리에이터가 되는 세상
+        </h1>
+        <h2 className='font-medium text-[38px] text-center mt-[40px] lg:text-[28px] md:text-[18px] md:mt-[28px]'>
+          그라운드엑스가 NFT로 새로운 <br className='hidden md:block' />
+          디지털 세상을 만들어갑니다.
+        </h2>
+      </div>
 
-        {/* 하단 Arrow Icon */}
-        <div className='absolute bottom-[20px] left-1/2 -translate-x-1/2'>
-          <div className='relative w-[48px] aspect-square animate-bounce'>
-            <Image
-              src='/home/section1/arrow.png'
-              alt='Main Arrow Icon'
-              layout='fill'
-            />
-          </div>
+      {/* 하단 Arrow Icon */}
+      <div className='absolute bottom-[20px] left-1/2 -translate-x-1/2'>
+        <div className='relative w-[48px] aspect-square animate-bounce'>
+          <Image
+            src='/home/section1/arrow.png'
+            alt='Main Arrow Icon'
+            layout='fill'
+          />
         </div>
       </div>
     </div>
